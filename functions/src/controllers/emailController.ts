deb52152ad0eb09359d46e601c56505e3eec13ec
Jonatan Morales Torres <jonyd@MacBook-Pro-de-Jonatan.local>
2021-02-24 01:10:02 -0600

    Correcciones por ESlin

--- functions/src/controllers/emailController.ts
+++ functions/src/controllers/emailController.ts
@@ -1,66 +1,65 @@
 import * as functions from "firebase-functions";
-import * as nodemailer from 'nodemailer';
+import * as nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-      user: 'contacto@bamboohogar.mx',
-      pass: 'OminumZ2020'
-    }
+        user: "contacto@bamboohogar.mx",
+        pass: "OminumZ2020",
+    },
 });
 
 transporter.verify(function(error: any) {
     if (error) {
         functions.logger.info(error);
     } else {
-        functions.logger.info('Server is ready to take our messages');
+        functions.logger.info("Server is ready to take our messages");
     }
 });
 
 export const send = functions.https.onRequest(async (request, response) => {
-
     try {
         functions.logger.info("Email:Send. Start");
-        
+
         if (request.method !== "POST" && request.is("application/json")) {
             throw new functions.https.HttpsError(
                 "unimplemented",
                 "Method not allowed");
         }
+
         if (request.body === undefined ) {
             throw new functions.https.HttpsError(
                 "invalid-argument",
                 "Data is empty");
         }
-        
+
         const { property, type, contact, description } = request.body;
 
-        var mail = {
-            from: 'contacto@bamboohogar.mx',
-            to: 'contacto@bamboohogar.mx',
+        const mail = {
+            from: "contacto@bamboohogar.mx",
+            to: "contacto@bamboohogar.mx",
             subject: `Interesados en ${property}`,
             html: `<h1> Interesado en la propiedad ${ property }  </h1>` +
-                '<hr />' + 
-                '<ul>' +
+                "<hr />" +
+                "<ul>" +
                     `<li> Quiere ser contactado con ${ type } </li>` +
                     `<li> Su contacto ${ contact } </li>` +
                     `<li> Descripción de lo que pide: ${ description } </li>` +
-                '</ul>'
-        }
-        
-        transporter.sendMail(mail, (err: any) => {
-        if (err) {
-            response.json({
-                status: 'fail'
-            })
-        } else {
-            response.json({
-                status: 'success'
-            })
-        }
-        });
+                "</ul>",
+        };
 
-    }catch(error){
+        transporter.sendMail(mail, (err: any) => {
+            if (err) {
+                response.json({
+                    status: "fail",
+                });
+            } else {
+                response.json({
+                    status: "success",
+                });
+            }
+        });
+    } catch ( error ) {
         functions.logger.error("Exception in Email:Send. ", error);
         if (error.message) {
             response.status(500).json({
